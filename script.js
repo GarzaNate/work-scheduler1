935b2c59d52c27674f5fcfc12c196610f09bd054
slendyG <josuegarza0608@gmail.com>
2022-08-07 00:11:09 -0700

    Added a function to change the background color of the row when the hour matches the current time.

--- script.js
+++ script.js
@@ -1,17 +1,51 @@
-console.log('alive');
+// work day scheduler project
+// getting each time block
+var timeblock = $("#row");
+var today = moment();
+console.log(today.format("MMM, Do, YYYY"));
+var currentTime = today.format("MMM, Do, YYYY, H, S, M");
+var currentHour = today.format("H");
+
+
+// still not displaying time in header
+$('#current-clock').innerHtml = currentTime;
 
 // Moment.js for calendar clock
-var calendar = moment();
-var today = moment();
-$("#current-clock").text(calendar.format("MMM Do, YYYY"));
-
-// console.log(calendar);
-// $('#current-clock').text(calendar.toString());
 
 
-// Time slot for each hour
 // Color differences for recent, current, and future events
-// Save changes to events to local storage
+// function for color class changes
+
+// function not yet working
+var colorSwitch = function() {
+    for (let i = 9; i < timeblock.length; i++) {
+        // if i = the same hour of the day, red
+        if (i = currentHour) {
+            function changeRed(event) {
+                event.timeblock.setAttribute("style", "background-color: #ff0000")
+            }
+        } else {
+            timeblock.setAttribute("style", "background-color: #90ee90")
+        }
+        // else, than green
+        // if past hour, color grey
+    }
+}
+
+// Save changes of events to local storage
+// Save button on.click
+// once user makes edit, save button saves to local storage
     // On click, be able to edit and save changes
     // Save button
 
+
+
+
+// style changing function
+    // function changePurple(event) {
+    //     // event.stopPropagation();
+    //     event.currentTarget.setAttribute(
+    //       "style",
+    //       "background-color: #601A4A"
+    //     );
+    //   }
