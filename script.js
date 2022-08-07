e0b2d9cc866445bc0034be1523ff8a785926f790
slendyG <josuegarza0608@gmail.com>
2022-08-06 23:03:38 -0700

    Working on adding a current day clock for the header of the scheduler

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log('alive');
+
+// Moment.js for calendar clock
+var calendar = moment();
+var today = moment();
+$("#current-clock").text(calendar.format("MMM Do, YYYY"));
+
+// console.log(calendar);
+// $('#current-clock').text(calendar.toString());
+
+
+// Time slot for each hour
+// Color differences for recent, current, and future events
+// Save changes to events to local storage
+    // On click, be able to edit and save changes
+    // Save button
+
