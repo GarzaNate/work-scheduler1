d5236ab96547add38ca94ad80f830900facb46ca
slendyG <josuegarza0608@gmail.com>
2022-08-07 22:26:55 -0700

    Tried a different way to get the color change effect to take place, also working on clock display.

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
 // work day scheduler project
 // getting each time block
-var timeblock = $("#row");
+var timeblock = $("#timeblock");
 var today = moment();
 console.log(today.format("MMM, Do, YYYY"));
-var currentTime = today.format("MMM, Do, YYYY, H, S, M");
+var currentTime = today.format("MMM, Do, YYYY");
 var currentHour = today.format("H");
 
 
 // still not displaying time in header
-$('#current-clock').innerHtml = currentTime;
-
-// Moment.js for calendar clock
-
+// $('#current-clock').innerHtml = currentTime;
+$("#current-clock").text(today.format("MMM Do, YYYY"));
 
 // Color differences for recent, current, and future events
 // function for color class changes
 
 // function not yet working
-var colorSwitch = function() {
+var colorSwitch = function(event) {
     for (let i = 9; i < timeblock.length; i++) {
         // if i = the same hour of the day, red
         if (i = currentHour) {
-            function changeRed(event) {
-                event.timeblock.setAttribute("style", "background-color: #ff0000")
-            }
+            event.timeblock.addClass("present")
+            // function changeRed(event) {
+            //     event.timeblock.setAttribute("style", "background-color: #ff0000")
+            // }
+        } else if (i < currentHour){
+            event.timeblock.addClass("past")
         } else {
-            timeblock.setAttribute("style", "background-color: #90ee90")
+            event.timeblock.addClass("future")
         }
-        // else, than green
-        // if past hour, color grey
     }
 }
+
+// on.click to let user edit an event
+
 
 // Save changes of events to local storage
 // Save button on.click
